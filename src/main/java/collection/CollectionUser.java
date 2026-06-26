64455c9d262bbfa4fc8e6c033615e6e6c3902edd
danypevzner <86599897+danypevzner@users.noreply.github.com>
2026-06-26 19:39:33 +0300

    Первоначальная структура проекта

---
+++ src/main/java/collection/CollectionUser.java
@@ -0,0 +1,10 @@
+package collection;
+
+import model.Car;
+
+public class CollectionUser {
+    //public Car addCar(Car car){}
+
+    //public Collection getSotredCars(){}
+
+}
---
+++ src/main/java/util/FieldValificator.java
@@ -0,0 +1,15 @@
+package util;
+
+public class FieldValificator {
+
+    public static boolean validatePower(int power){
+        return false;
+    };
+    public static boolean validateModel(String model){
+        return false;
+    }
+    public static boolean validateYear(String year){
+        return false;
+    }
+
+}
