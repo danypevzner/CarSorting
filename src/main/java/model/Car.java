ec44d30e8909f4439f89a6454b90066b12aecb81
Anna <annstr.94@gmail.com>
2026-07-02 14:36:25 +0300

    Добавлен класс Car с полями, геттерами и toString

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Car {
+    private String model;
+    private int power;
+    private int year;
+
+    public Car(String model, int power, int year) {
+        this.model = model;
+        this.power = power;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public int getPower() {
+        return power;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return getModel() + " " + getPower() + " " + getYear();
+    }
 }
