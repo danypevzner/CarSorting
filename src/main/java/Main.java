12a9dd0012ad9e34e20a9fa746f30ee18d7b861f
danypevzner <86599897+danypevzner@users.noreply.github.com>
2026-07-04 21:59:34 +0300

    Переписал под CSV формат данных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,10 @@
-import com.fasterxml.jackson.core.type.TypeReference;
 import model.Car;
 import util.FileUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Car> carList = new ArrayList<Car>();
-        carList.add(new Car("Tesla",300,2000));
-        carList.add(new Car("Honda",140,2020));
-        FileUtil.writeCarList("C:\\Users\\Daniil Pevzner\\IdeaProjects\\CarSorting\\src\\main\\files\\carsJson",carList);
-        ArrayList<Car> carListRead;
-        carListRead= FileUtil.readCarList("C:\\Users\\Daniil Pevzner\\IdeaProjects\\CarSorting\\src\\main\\files\\carsJson");
-        System.out.println(carListRead.get(0).getModel());
-        System.out.println(carListRead.get(1).getModel());
-
+    public static void main(String[] args) throws IOException {
+        FileUtil.appendFile(new Car("Mustang",200,1990),"C:\\Users\\Daniil Pevzner\\IdeaProjects\\CarSorting\\src\\main\\files\\CarsCSV");
     }
 }
--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -1,68 +1,89 @@
 package util;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Car;
 
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtil {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static ArrayList<Car> readCarList(String filePath){
-        return  readObjectFromFile(filePath,new TypeReference<ArrayList<Car>>() {});
-    }
+    public static List<Car> readFile(String filepath) throws IOException {
+        List<Car> result = new ArrayList<>();
+        Path path = Path.of(filepath);
 
-    public static void writeCarList(String filePath,ArrayList<Car> carList){
-        saveObjectToFile(filePath,carList);
-    }
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 
-    public static void  addCarToFile(String filePath,Car car){
-        ArrayList<Car> carList = readCarList(filePath);
-        carList.add(car);
-        writeCarList(filePath,carList);
-    }
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i).trim();
 
-    public static void addCarListToFile(String filePath,ArrayList<Car> carList){
-        ArrayList<Car> carListCurrent = readCarList(filePath);
-        carListCurrent.addAll(carList);
-        writeCarList(filePath,carListCurrent);
-    }
-
-
-
-    private static <T> T readObjectFromFile(String filepath, TypeReference<T> typeRef) {
-        try {
-            if (filepath == null || filepath.isBlank()) {
-                throw new IllegalArgumentException("Путь к файлу не может быть пустым");
+            if (line.isEmpty()) {
+                continue;
             }
-            Path path = Path.of(filepath);
-            String jsonString = Files.readString(path);
-            return objectMapper.readValue(jsonString, typeRef);
-        } catch (IOException e) {
-            throw new RuntimeException("Ошибка чтения файла: " + filepath, e);
-        }
-    }
 
-    public static void saveObjectToFile(String filepath,Object object){
-        if (filepath == null || filepath.isBlank()) {
-            throw new IllegalArgumentException("Путь к файлу не может быть пустым");
-        }
-        try {
-            File file = new File(filepath);
-            if (!file.getParentFile().exists()) {
-                file.getParentFile().mkdirs();
+            String[] values = line.split(";", 0);
+
+            if (values.length < 3) {
+                throw new IOException("Некорректная строка - недостаточно полей:" + line);
             }
-            objectMapper.writeValue(file, object);
-        } catch (IOException e) {
-            throw new RuntimeException("Ошибка записи в файл: " + filepath, e);
+
+            try {
+                String model = values[0].trim();
+                int year = Integer.parseInt(values[1].trim());
+                int price = Integer.parseInt(values[2].trim());
+
+                result.add(new Car(model, year, price));
+            } catch (NumberFormatException e) {
+                throw new IOException("Ошибка парсинга строки:"+line);
+            }
+        }
+
+        return result;
+    }
+
+    public static void  writeFile(List<Car> carList,String filepath) throws IOException {
+        Path path = Path.of(filepath);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING)) {
+
+            for (Car car : carList) {
+                String line = car.getModel() + ";" + car.getPower() + ";" + car.getYear();
+                writer.write(line);
+                writer.newLine();
+            }
         }
     }
 
-    private FileUtil(){};
+    public static void  appendFile(List<Car> carList,String filepath) throws IOException {
+        Path path = Path.of(filepath);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND)) {
+
+            for (Car car : carList) {
+                String line = car.getModel() + ";" + car.getPower() + ";" + car.getYear();
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+    }
+
+    public static void  appendFile(Car car,String filepath) throws IOException {
+        Path path = Path.of(filepath);
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND)) {
+                String line = car.getModel() + ";" + car.getPower() + ";" + car.getYear();
+                writer.write(line);
+                writer.newLine();
+        }
+    }
+
+
+
+
+
 
 }
