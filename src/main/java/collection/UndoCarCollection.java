1c612426e82981aa760b40560a458be32f967105
Anna <annstr.94@gmail.com>
2026-07-10 18:04:31 +0300

    Рефакторинг: отмена последнего действия вынесена в UndoCarCollection

---
+++ src/main/java/collection/UndoCarCollection.java
@@ -0,0 +1,55 @@
+package collection;
+
+import model.Car;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class UndoCarCollection extends CarCollection {
+    private final Deque<CarCollection> history = new ArrayDeque<>();
+    private static final int MAX_HISTORY = 10;
+
+    @Override
+    public boolean add(Car car) {
+        saveState();
+        return super.add(car);
+    }
+
+    @Override
+    public Car remove(int index) {
+        saveState();
+        return super.remove(index);
+    }
+
+    @Override
+    public boolean addAll(java.util.Collection<? extends Car> cars) {
+        saveState();
+        return super.addAll(cars);
+    }
+
+    @Override
+    public void clear() {
+        saveState();
+        super.clear();
+    }
+
+    private void saveState() {
+        CarCollection snapshot = new CarCollection();
+        snapshot.addAll(this);
+        history.push(snapshot);
+        if (history.size() > MAX_HISTORY) {
+            history.pollLast();
+        }
+    }
+
+    public boolean undo() {
+        if (history.isEmpty()) {
+            return false;
+        }
+        CarCollection previous = history.pop();
+        this.clear();
+        this.addAll(previous);
+        return true;
+    }
+}
